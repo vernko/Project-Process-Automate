561c6a95a80cdb87295c0b5fcff368a568c576b2
Vern Kofford <cheow3@gmail.com>
2018-06-23 01:27:19 -0600

    Created all automated inputs for Discover site, and ability to run automation. Created forms page to begin work on landing page. Started set up for getting Salesforce data

---
+++ discover.rb
@@ -0,0 +1,173 @@
+require 'selenium-webdriver'
+require 'faker'
+require_relative 'baseclass'
+
+class Discover < BaseClass
+  def input_intial_info
+    first_name_field = @driver.find_element(:xpath => "//input[@name='firstName']")
+    type_things(first_name_field, @first_name)
+    sleep 1
+
+    last_name_field = @driver.find_element(:xpath => "//input[@name='lastName']")
+    type_things(last_name_field, @last_name)
+    sleep 1
+
+    pick_card_design = @driver.find_element(:xpath => "//a[@class='card_thumb selected']")
+    pick_card_design.click
+
+    continue_button = @driver.find_element(:xpath => "//input[@title='Continue']")
+    continue_button.click
+    sleep 2
+  end
+
+  def input_general_info
+    mailing_address_field = @driver.find_element(:xpath => "//input[@name='homeAddress1']")
+    type_things(mailing_address_field, @mailing_address)
+    sleep 1
+
+    city_field = @driver.find_element(:xpath => "//input[@name='city']")
+    type_things(city_field, @city)
+    sleep 1
+
+    state_selector_list = @driver.find_element(:xpath => "//select[@name='state']")
+    options = state_selector_list.find_elements(:tag_name => "option")
+    options.each do |g|
+      if g.text == "Utah"
+      g.click
+      break
+      end
+    end
+    sleep 1
+
+    zipcode_field = @driver.find_element(:xpath => "//input[@name='zipCode']")
+    type_things(zipcode_field, @zipcode)
+    sleep 1
+
+    email_field = @driver.find_element(:xpath => "//input[@name='email']")
+    type_things(email_field, @email)
+    sleep 1
+
+    primary_phone_field = @driver.find_element(:xpath => "//input[@name='homePhone']")
+    type_things(primary_phone_field, @primary_phone)
+    sleep 1
+    
+    dob_month_field = @driver.find_element(:xpath => "//input[@name='dobMonth']")
+    type_things(dob_month_field, @dob)
+    # may need additional for separate day & year fields
+    # dob_day_field = @driver.find_element(:xpath => "//input[@name='dobDay']")
+    # type_things(dob_day_field, @dob)
+    # dob_year_field = @driver.find_element(:xpath => "//input[@name='dobYear']")
+    # type_things(dob_year_field, @dob)
+    sleep 1
+
+    social_field = @driver.find_element(:xpath => "//input[@name='socialSecurity']")
+    type_things(social_field, @social)
+    sleep 1
+
+    income_source_selector_list = @driver.find_element(:xpath => "//select[@name='employmentStatus']")
+    options = income_source_selector_list.find_elements(:tag_name => "option")
+    options.each do |g|
+      if g.text == "Employed Full-Time"
+      g.click
+      break
+      end
+    end
+    sleep 1
+
+    gross_income = @driver.find_element(:xpath => "//input[@name='income']")
+    type_things(gross_income, @gross_income)
+    sleep 1
+
+    housing_payment = @driver.find_element(:xpath => "//input[@name='housingCost']")
+    type_things(housing_payment, @housing_payment)
+
+    total_assets = @driver.find_element(:xpath => "//input[@name='totalAssets']")
+    type_things(total_assets, @total_assets)
+
+    continue_button = @driver.find_element(:xpath => "//input[@title='Continue']")
+    continue_button.click
+    # asks to verify, click again that input email correctly if system not sure of domain
+    sleep 2
+  end
+
+  def input_final_info
+    sleep 1
+    occupation_selector_list = @driver.find_element(:xpath => "//select[@class='occupation']")
+    options = occupation_selector_list.find_elements(:tag_name => "option")
+    options.each do |g|
+      if g.text == "ADMINISTRATIVE/OFFICE SUPP"
+      g.click
+      break
+      end
+    end
+    sleep 1
+    
+    employer = @driver.find_element(:xpath => "//input[@name='employer']")
+    type_things(employer, @employer)
+    sleep 1
+
+    # length of employment is in number of years
+    employment_time = @driver.find_element(:xpath => "//input[@name='employLength']")
+    type_things(employment_length, @employment_length)
+    sleep 1
+
+    highest_education_level = @driver.find_element(:xpath => "//select[@name='education']")
+    options = occupation_selector_list.find_elements(:tag_name => "option")
+    options.each do |g|
+      if g.text == "Bachelor's Degree"
+      g.click
+      break
+      end
+    end
+    sleep 1
+    sleep 1
+
+    # residence_selector
+    # if select rent or other brings up Monthy Housing Payment field
+    housing_type = @driver.find_element(:xpath => "//select[@name='value(housingStatus)']")
+    options = housing_type.find_elements(:tag_name => "option")
+    options.each do |g|
+      if g.text == "Own Home"
+      g.click
+      break
+      end
+    end
+    sleep 1
+
+    current_residence_length = @driver.find_element(:xpath => "//input[@name='currentResidenceLength']")
+    type_things(current_residence_length, @current_residence_length)
+    sleep 1
+
+    checking_debit_radio_button = @driver.find_element(:xpath => "//input[@name='checkingOrDebit']")
+    checking_debit_radio_button.click
+
+    savings_ira_radio_button = @driver.find_element(:xpath => "//input[@name='savingsIraMoneyMarket']")
+    savings_ira_radio_button.click
+
+    us_citizen_radio_button = @driver.find_element(:xpath => "//input[@name='citizen']")
+    us_citizen_radio_button.click
+
+    mothers_maiden_name_field = @driver.find_element(:xpath => "//input[@name='mothersMaidenName']")
+    type_things(mothers_maiden_name_field, @mothers_maiden_name)
+    sleep 1
+
+    continue_button = @driver.find_element(:xpath => "//input[@title='Continue']")
+    continue_button.click
+    sleep 2
+  end
+
+  def review_submit
+    consent_radio_button = @driver.find_element(:xpath => "//input[@name='eSignCheckbox']")
+    consent_radio_button.click
+
+    terms_agreement_box = @driver.find_element(:xpath => "//input[@name='termsCondsCheckbox']")
+    terms_agreement_box.click
+
+    submit_button = @driver.find_element(:xpath => "//button[@id='terms-conditions-submit']")
+    submit_button.click
+    sleep 10
+    
+    @driver.screenshot_as(:png)
+    @driver.save_screenshot('status.png')
+  end
+end
--- discover_spec.rb
+++ discover_spec.rb
@@ -2,9 +2,9 @@ require 'selenium-webdriver'
 require 'rspec'
 require_relative 'chasefreedom'
 
-describe "Input initial information in Chase application" do
+describe "Fill out Discover's card application" do
   before(:each) do
-    @browser = ChaseFreedom.new
+    @browser = Discover.new
     @browser.setup
   end
 
@@ -13,9 +13,9 @@ describe "Input initial information in Chase application" do
   end
 
   it 'inputs information' do
-    @browser.personal_info
-    @browser.financial_info
-    @browser.security_info
+    @browser.input_intial_info
+    @browser.input_general_info
+    @browser.input_final_info
     @browser.review_submit
   end
 end
